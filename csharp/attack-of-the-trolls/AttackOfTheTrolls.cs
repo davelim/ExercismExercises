969e2ea731645a1dd625b4a3d9bc7b0208aa93d2
Dave Lim <dave.lim@qbcinc.com>
2024-09-17 10:45:37 -0500

    Attack of the trolls, task 1
    Add AccountType and Permission enum. Implement Permissions.Default().

--- csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
+++ csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,14 +1,45 @@
 using System;
 
 // TODO: define the 'AccountType' enum
+[Flags]
+enum AccountType: byte
+{
+    Guest     = 0b00000001,
+    User      = 0b00000010,
+    Moderator = 0b00000100
+}
 
 // TODO: define the 'Permission' enum
+[Flags]
+enum Permission: byte
+{
+    Read   = 0b00000001,
+    Write  = 0b00000010,
+    Delete = 0b00000100,
+    All    = 0b00000111,
+    None   = 0b00000000
+}
 
 static class Permissions
 {
     public static Permission Default(AccountType accountType)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
+        Permission perm = Permission.None;
+        switch (accountType)
+        {
+            case AccountType.Guest:
+                perm = Permission.Read;
+                break;
+            case AccountType.User:
+                perm = Permission.Read | Permission.Write;
+                break;
+            case AccountType.Moderator:
+                perm = Permission.All;
+                break;
+            default:
+                break;
+        }
+        return perm;
     }
 
     public static Permission Grant(Permission current, Permission grant)
