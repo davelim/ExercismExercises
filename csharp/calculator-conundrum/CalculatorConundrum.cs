86433206fb2af2cd624d90c346252a04676fe321
Dave Lim <dave.lim@qbcinc.com>
2024-09-17 16:44:29 -0500

    Calculator conundrum, task 1
    Implement calculator operations.

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -4,6 +4,23 @@ public static class SimpleCalculator
 {
     public static string Calculate(int operand1, int operand2, string operation)
     {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
+        int result = 0;
+        switch (operation)
+        {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                result = operand1 / operand2;
+                break;
+            default:
+                throw new ArgumentException($"'{operation}' is not supported",
+                    "operation");
+                break;
+        }
+        return $"{operand1} {operation} {operand2} = {result}";
     }
 }
