e86fb514666d32242d1c6933046f1b39750ab4c2
Dave Lim <dave.lim@qbcinc.com>
2024-09-18 16:13:21 -0500

    Log analysis, task 1
    Implement LogAnalysis.SubstringAfter() which returns the substring after
    a specified delimiter.

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -2,7 +2,11 @@ using System;
 
 public static class LogAnalysis 
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    public static string SubstringAfter(this string log, string delimiter)
+    {
+        int startIndex = log.IndexOf(delimiter);
+        return startIndex == -1 ? log : log.Substring(startIndex+delimiter.Length);
+    }
 
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     
