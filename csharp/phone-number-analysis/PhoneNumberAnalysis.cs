68e0c850f4314c8420b6eaeb05070e9c2fb6d513
Dave Lim <dave.lim@qbcinc.com>
2024-09-18 15:37:02 -0500

    Phone number analysis, task 1
    Implement PhoneNumber.Analyze().

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,7 +4,9 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        bool isNewYork = phoneNumber.StartsWith("212");
+        bool isFake = phoneNumber.Substring(4, 3).Equals("555");
+        return (isNewYork, isFake, phoneNumber.Substring(8));
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
