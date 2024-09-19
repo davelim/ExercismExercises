93ab08e66f27c25ea7d1d1bab57a87ba2b0c0666
Dave Lim <dave.lim@qbcinc.com>
2024-09-19 10:59:54 -0500

    Sum of multiples, calculate energy points

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        HashSet<int> numbers = new();
+        foreach (var baseValue in multiples) {
+            if (baseValue <= 0)
+                continue;
+            var i = 0;
+            int multiple;
+            while ((multiple = baseValue * i++) < max)
+            {
+                numbers.Add(multiple);
+            }
+        }
+        return numbers.Sum(n => n);
     }
 }
