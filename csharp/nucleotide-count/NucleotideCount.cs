5f12cc76d1d4baa77e824e941ea679aacf4fbe46
Dave Lim <dave.lim@qbcinc.com>
2024-09-19 12:02:50 -0500

    Nucleotide count, task 1
    Implement NucleotideCount.Count() which counts the number of each
    nucleotide present in a specified sequence.

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (InvalidSequence(sequence))
+            throw new ArgumentException(
+                $"'{sequence}' is an invalid sequence.", "sequence");
+
+        var nucleotideCount = new Dictionary<char, int> {
+            {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}
+        };
+        foreach (var c  in sequence)
+        {
+            nucleotideCount[c]++;
+        }
+        return nucleotideCount;
+    }
+
+    private static bool InvalidSequence(string sequence)
+    {
+        var pattern = "[^ACGT]";
+        return Regex.IsMatch(sequence, pattern);
     }
 }
