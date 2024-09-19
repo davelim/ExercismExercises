022d0e4e54a15487189c8b7c118c80b0c6294c99
Dave Lim <dave.lim@qbcinc.com>
2024-09-19 12:26:38 -0500

    RNA transcription
    Implement RnaTranscription.ToRna() which transcribes an RNA strand from
    a specified DNA strand.

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class RnaTranscription
 {
+    private static Dictionary<char, char> _dnaToRna = new Dictionary<char, char> {
+        {'G', 'C'}, {'C', 'G'}, {'T', 'A'}, {'A', 'U'}
+    };
     public static string ToRna(string strand)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return string.Join("",strand.Select(n => DnaToRna[n]));
     }
 }
