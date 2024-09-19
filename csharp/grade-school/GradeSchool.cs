751510bdb4a6c4ff8d177adc31f1a206ca6220e9
Dave Lim <dave.lim@qbcinc.com>
2024-09-19 14:29:45 -0500

    GradeSchool.Roster()
    Method that returns school roster sorted by grade, then student name.

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private Dictionary<int, HashSet<string>> _gradesWithStudents = new();
+
     public bool Add(string student, int grade)
     {
         throw new NotImplementedException("You need to implement this method.");
@@ -10,7 +13,14 @@ public class GradeSchool
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        List<string> roster = new();
+        var sortedByGrades = _gradesWithStudents
+            .OrderBy(pair => pair.Key)
+            .ToDictionary(pair => pair.Key, pair => pair.Value);
+        foreach (var grade in sortedByGrades.Values) {
+            roster.AddRange(grade.OrderBy(name => name));
+        }
+        return roster;
     }
 
     public IEnumerable<string> Grade(int grade)
