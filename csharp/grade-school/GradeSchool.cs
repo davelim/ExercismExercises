e61a096fec7bd5e038e9bd853a687077242f97c2
Dave Lim <dave.lim@qbcinc.com>
2024-09-19 16:58:59 -0500

    GradeSchool, slightly different implementation
    Slightly different implementation for better memory foot print and
    readability (maybe). Also, a chance to use aliases. :p

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -2,38 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using Student = (string Name, int Grade);
 public class GradeSchool
 {
-    private Dictionary<string, int> _students = new();
+    private List<Student> _students = new();
 
     public bool Add(string student, int grade)
     {
-        try
-        {
-            _students.Add(student, grade);
-            return true;
-        }
-        catch(ArgumentException)
-        {
+        if (_students.Select(student => student.Name).Contains(student))
             return false;
-        }
+        _students.Add((student, grade));
+        return true;
     }
 
     public IEnumerable<string> Roster()
     {
         return _students
-            .OrderBy(student => student.Value)
-            .ThenBy(student => student.Key)
-            .Select(student => student.Key)
+            .OrderBy(student => student.Grade)
+            .ThenBy(student => student.Name)
+            .Select(student => student.Name)
             .ToArray();
     }
 
     public IEnumerable<string> Grade(int grade)
     {
         return _students
-            .Where(student => student.Value == grade)
-            .Select(student => student.Key)
-            .OrderBy(student => student)
+            .Where(student => student.Grade == grade)
+            .OrderBy(student => student.Name)
+            .Select(student => student.Name)
             .ToArray();
     }
 }
