068d7f2d48fa7485964645eb24bb9a3a5d41cc4b
Dave Lim <dave.lim@qbcinc.com>
2024-09-24 17:18:58 -0500

    Tisbury treasure hunt, task 3
    Implement compareRecords() which returns true if coordinates of given
    data match.

--- fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -2,12 +2,16 @@ module TisburyTreasureHunt
 
 let getCoordinate (line: string * string): string = snd line
 
+// TODO: function currently only handles happy case. What error cases should
+//   function account for?
 let convertCoordinate (coordinate: string): int * char =
     // https://stackoverflow.com/questions/42820232/f-convert-a-char-to-int
     int coordinate[0] - int '0', coordinate[1]
 
-let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
-    failwith "Please implement the 'compareRecords' function"
+let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool =
+    let azarasCoordinate = azarasData |> getCoordinate |> convertCoordinate
+    let (_, ruisCoordinate, _) = ruisData
+    ruisCoordinate = azarasCoordinate
 
 let createRecord (azarasData: string * string) (ruisData: string * (int * char) * string) : (string * string * string * string) =
     failwith "Please implement the 'createRecord' function"
