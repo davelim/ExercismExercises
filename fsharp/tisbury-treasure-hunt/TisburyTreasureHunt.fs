6e9c8953081f575c5e91023f9454495e2de72874
Dave Lim <dave.lim@qbcinc.com>
2024-09-24 16:51:37 -0500

    Tisbury treasure hunt, task 2
    Implement convertCoordinate() which returns the coordinate as a tuple
    (int * char) when given coordinate as a string.
    
    TODO: function currently only handles happy case. What error cases
    should function account for?

--- fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -2,8 +2,9 @@ module TisburyTreasureHunt
 
 let getCoordinate (line: string * string): string = snd line
 
-let convertCoordinate (coordinate: string): int * char = 
-    failwith "Please implement the 'convertCoordinate' function"
+let convertCoordinate (coordinate: string): int * char =
+    // https://stackoverflow.com/questions/42820232/f-convert-a-char-to-int
+    int coordinate[0] - int '0', coordinate[1]
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
     failwith "Please implement the 'compareRecords' function"
