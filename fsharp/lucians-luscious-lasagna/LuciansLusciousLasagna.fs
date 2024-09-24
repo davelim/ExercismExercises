bfef05723208f71fbef6194e695eb24f99e24e51
Dave Lim <dave.lim@qbcinc.com>
2024-09-24 12:31:59 -0500

    Lucian's luscious lasagna, task 1
    Define expectedMinutesInOven, bind to 42.

--- fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -1,9 +1,12 @@
 module LuciansLusciousLasagna
 
-// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
 
 // TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven x = 42
 
 // TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes x = 42
 
 // TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes x y = 42
