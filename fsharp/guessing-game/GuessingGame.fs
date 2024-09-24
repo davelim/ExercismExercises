b4f9cffcd1b19550a01e5dd1424c96c30c9bc6ea
Dave Lim <dave.lim@qbcinc.com>
2024-09-24 13:56:32 -0500

    Guessing game, implement reply()
    Resources:
    - https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/pattern-matching
    - https://learn.microsoft.com/en-us/dotnet/fsharp/language-reference/match-expressions

--- fsharp/guessing-game/GuessingGame.fs
+++ fsharp/guessing-game/GuessingGame.fs
@@ -1,3 +1,8 @@
 module GuessingGame
 
-let reply (guess: int): string = failwith "Please implement the 'reply' function"
+let reply (guess: int): string =
+    match guess with
+    | 42  -> "Correct"
+    | 41 | 43 -> "So close"
+    | i when i < 41 -> "Too low"
+    | i when i > 43 -> "Too high"
