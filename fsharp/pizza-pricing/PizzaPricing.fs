6590bdf6978bc99e467a2f2343c2ad88ab9fdfe8
Dave Lim <dave.lim@qbcinc.com>
2024-09-25 10:24:45 -0500

    Pizza pricing, task 3
    Implement orderPrice() which calculates price of specified order (list
    of pizzas).
    
    Concepts:
    - recursion function on recursive type
    - List.fold
    Resources:
    - https://fsharpforfunandprofit.com/posts/recursive-types-and-folds/#basic-recursive-type
      - through "Working with recursive type"
    - https://fsharp.github.io/fsharp-core-docs/reference/fsharp-collections-listmodule.html
      - see List.iter, List.map, List.fold, List.scan

--- fsharp/pizza-pricing/PizzaPricing.fs
+++ fsharp/pizza-pricing/PizzaPricing.fs
@@ -15,4 +15,10 @@ let rec pizzaPrice (pizza: Pizza): int =
     | ExtraSauce pizza -> 1 + pizzaPrice pizza
     | ExtraToppings pizza -> 2 + pizzaPrice pizza
 
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let orderPrice(pizzas: Pizza list): int =
+    let startPrice =
+        match List.length pizzas with
+        | 1 -> 3
+        | 2 -> 2
+        | _ -> 0
+    (startPrice, pizzas) ||> List.fold (fun accPrice pizza -> accPrice + pizzaPrice pizza)
