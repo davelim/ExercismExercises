9fca421ec799f56f5b99b6f58950e8e30fb0a40c
Dave Lim <dave.lim@qbcinc.com>
2024-09-25 09:48:47 -0500

    Pizza pricing, task 1 & 2
    Define supported pizza types and function to calculate pizza price of
    specified pizza.

--- fsharp/pizza-pricing/PizzaPricing.fs
+++ fsharp/pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,18 @@
 module PizzaPricing
 
-// TODO: please define the 'Pizza' discriminated union type
+type Pizza =
+    | Margherita
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
+let rec pizzaPrice (pizza: Pizza): int =
+    match pizza with
+    | Margherita -> 7
+    | Caprese -> 9
+    | Formaggio -> 10
+    | ExtraSauce pizza -> 1 + pizzaPrice pizza
+    | ExtraToppings pizza -> 2 + pizzaPrice pizza
 
 let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
