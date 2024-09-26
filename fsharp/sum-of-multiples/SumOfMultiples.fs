a21312fd4d9eb3d5c73348997f219686e3f8b83b
Dave Lim <dave.lim@qbcinc.com>
2024-09-26 12:20:44 -0500

    Sum of multiples, sum()

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,3 +1,13 @@
 module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = failwith "You need to implement this function."
+let addItem upperBound acc itemBase =
+    match itemBase with
+    | 0 -> acc
+    | _ -> acc + Set(seq{itemBase..itemBase..upperBound})
+
+let sumSet set =
+    (0, set) ||> Set.fold(fun acc n -> acc + n)
+
+let sum (numbers: int list) (upperBound: int): int =
+    let addItemBakedUpperBound = addItem (upperBound - 1)
+    (Set.empty, numbers) ||> List.fold addItemBakedUpperBound |> sumSet
