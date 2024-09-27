d6ae79ac71691a4b680f1ef7c29ed5cffa508e48
davelim <dave.lim@ekonbenefits.com>
2024-09-27 12:31:30 -0500

    Sum of multiple, update based on review
    For readability, make functions local, one line per list operation.

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,10 +1,14 @@
 module SumOfMultiples
 
-let addItem upperBound itemBase =
-    match itemBase with
-    | 0 -> []
-    | _ -> [itemBase..itemBase..upperBound]
-
 let sum (numbers: int list) (upperBound: int): int =
+    let addItem upperBound itemBase =
+        match itemBase with
+        | 0 -> []
+        | _ -> [itemBase..itemBase..upperBound]
+
     let addItemBakedUpperBound = addItem (upperBound - 1)
-    numbers |> List.collect addItemBakedUpperBound |> List.distinct |> List.sum
+
+    numbers
+    |> List.collect addItemBakedUpperBound
+    |> List.distinct
+    |> List.sum
