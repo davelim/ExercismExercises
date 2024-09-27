8b52071be455bc26f6d0d18d078216b73db8466a
Dave Lim <dave.lim@qbcinc.com>
2024-09-27 09:23:34 -0500

    Nucleotide count function
    Implement nucleotideCounts() which counts how many of each nucleotide
    present in a specified strand.

--- fsharp/nucleotide-count/NucleotideCount.fs
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -1,3 +1,10 @@
 module NucleotideCount
+open System.Text.RegularExpressions
 
-let nucleotideCounts (strand: string): Option<Map<char, int>> =  failwith "You need to implement this function."
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+    let badStrand = Regex(@"[^ACGT]")
+    let countNucl (acc: Map<char, int>) char = acc.Add(char, acc[char]+1)
+    let empty = Map([('A', 0); ('C', 0); ('G', 0); ('T', 0)])
+    match strand with
+    | _ when badStrand.IsMatch(strand) -> None
+    | _ -> (empty, strand) ||> Seq.fold countNucl |> Some
