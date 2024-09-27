bd3fab74b2bd0658f5d3f6929226428024b8b25e
davelim <dave.lim@ekonbenefits.com>
2024-09-27 11:34:08 -0500

    Sum of multiples, update based on review
    To help with readability, instead of List.fold into Set<int>, do
    List.collect into List.distinct.

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,13 +1,10 @@
 module SumOfMultiples
 
-let addItem upperBound acc itemBase =
+let addItem upperBound itemBase =
     match itemBase with
-    | 0 -> acc
-    | _ -> acc + Set(seq{itemBase..itemBase..upperBound})
-
-let sumSet set =
-    (0, set) ||> Set.fold(fun acc n -> acc + n)
+    | 0 -> []
+    | _ -> [itemBase..itemBase..upperBound]
 
 let sum (numbers: int list) (upperBound: int): int =
     let addItemBakedUpperBound = addItem (upperBound - 1)
-    (Set.empty, numbers) ||> List.fold addItemBakedUpperBound |> sumSet
+    numbers |> List.collect addItemBakedUpperBound |> List.distinct |> List.sum
