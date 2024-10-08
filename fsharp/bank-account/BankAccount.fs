bd8b3ed77be68c01c51569a1b4566f2a5ccbf6bc
davelim <dave.lim@ekonbenefits.com>
2024-10-08 15:31:30 -0500

    Bank account, first cut
    Two tests fail:
    - "Balance can increment or decrement"
    - "Account can be updated from multiple threads"

--- fsharp/bank-account/BankAccount.fs
+++ fsharp/bank-account/BankAccount.fs
@@ -1,11 +1,46 @@
 module BankAccount
 
-let mkBankAccount() = failwith "You need to implement this function."
+type AccountData = { Balance: decimal }
+type Account =
+    | NewAccount
+    | OpenedAccount of AccountData
+    | ClosedAccount
+// TODO: keep balance around for ClosedAccount? Is Closed-to-Opened ok?
 
-let openAccount account = failwith "You need to implement this function."
+let mkBankAccount() = NewAccount
 
-let closeAccount account = failwith "You need to implement this function."
+let openAccount account =
+    match account with
+    | NewAccount ->
+        OpenedAccount {Balance = 0.0m}
+    | OpenedAccount _ ->
+        account
+    | ClosedAccount ->
+        account
 
-let getBalance account = failwith "You need to implement this function."
+let closeAccount account =
+    match account with
+    | NewAccount ->
+        account
+    | OpenedAccount _ ->
+        ClosedAccount
+    | ClosedAccount ->
+        account
 
-let updateBalance change account = failwith "You need to implement this function."
+let getBalance account =
+    match account with
+    | NewAccount ->
+        None
+    | OpenedAccount {Balance=balance} ->
+        Some balance
+    | ClosedAccount ->
+        None
+
+let updateBalance change account =
+    match account with
+    | NewAccount ->
+        account
+    | OpenedAccount {Balance=balance} ->
+        OpenedAccount {Balance = balance + change}
+    | ClosedAccount ->
+        account
