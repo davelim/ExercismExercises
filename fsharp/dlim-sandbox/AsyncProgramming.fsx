27f44c5f4c157f511683d671b2851a11734defa4
davelim <dave.lim@ekonbenefits.com>
2024-10-09 12:57:50 -0500

    F# concurrency, async programming
    Concepts:
    - IAsyncResult, e.g. stream.BeginWrite implements interface
    - async wrappers around IAsyncResult

--- fsharp/dlim-sandbox/AsyncProgramming.fsx
+++ fsharp/dlim-sandbox/AsyncProgramming.fsx
@@ -1,38 +1,64 @@
+module AsyncProgramming
+
 open System
 
-let userTimerWithCallback =
-    // create an event to wait on
-    let event = new System.Threading.AutoResetEvent(false)
+// let userTimerWithCallback =
+//     // create an event to wait on
+//     let event = new System.Threading.AutoResetEvent(false)
+//     // create a timer and add an event handler that will signal the "event"
+//     let timer = new System.Timers.Timer(2000.0)
+//     timer.Elapsed.Add (fun _ ->
+//         printfn "timer elapsed, \"signal\" event"
+//         event.Set() |> ignore
+//     )
+//     // start timer
+//     printfn "Waiting for timer at %O" DateTime.Now.TimeOfDay
+//     timer.Start()
+//     // keep working
+//     printfn "Doing something useful while waiting for event"
+//     printfn "working...1"
+//     System.Threading.Thread.Sleep(1000);
+//     printfn "working...2"
+//     // System.Threading.Thread.Sleep(1000);
+//     printfn "working...3"
+//     // System.Threading.Thread.Sleep(1000);
+//     printfn "working...4"
+//     // block on the timer via the AutoResetEvent
+//     event.WaitOne() |> ignore
+//     //done
+//     printfn "Timer ticked at %O" DateTime.Now.TimeOfDay
 
-    // create a timer and add an event handler that will signal the "event"
-    let timer = new System.Timers.Timer(2000.0)
-    // TODO: "AutoRest = false" doesn't prevent timer from firing again, why?
-    timer.AutoReset = false
-    timer.Elapsed.Add (fun _ ->
-        printfn "timer elapsed, \"signal\" event"
-        event.Set() |> ignore
-    )
 
-    // start timer
-    printfn "Waiting for timer at %O" DateTime.Now.TimeOfDay
-    timer.Start()
+// let userTimerWithAsync =
+//     // create a timer and associated async event
+//     let timer = new System.Timers.Timer(2000.0)
+//     // asynchronous workflow - object to encapsulate a background task
+//     let timerEvent = Async.AwaitEvent (timer.Elapsed) |> Async.Ignore
+//     // start
+//     printfn "Waiting for timer at %O" DateTime.Now.TimeOfDay
+//     timer.Start()
+//     // keep working
+//     printfn "Doing something useful while waiting for event"
+//     // block on the timer event now by waiting for the async to complete
+//     Async.RunSynchronously timerEvent
+//     // done
+//     printfn "Timer ticked at %O" DateTime.Now.TimeOfDay
 
+let fileWriteWithAsync =
+    // create a stream to write to
+    use stream = new System.IO.FileStream("test.txt",System.IO.FileMode.Create)
+    // start
+    printfn "Starting async write"
+    let buf =
+        "Hello!".ToCharArray()
+        |> Seq.map (fun c -> int c |> byte)
+        |> Seq.toArray
+    let asyncResult = stream.BeginWrite(buf,0,buf.Length,null,null)
+    // create an async wrapper around an IAsyncResult
+    let async = Async.AwaitIAsyncResult(asyncResult) |> Async.Ignore
     // keep working
-    printfn "Doing something useful while waiting for event"
-    printfn "working...1"
-    System.Threading.Thread.Sleep(1000);
-    printfn "working...2"
-    // System.Threading.Thread.Sleep(1000);
-    printfn "working...3"
-    // System.Threading.Thread.Sleep(1000);
-    printfn "working...4"
-
-    // block on the timer via the AutoResetEvent
-    event.WaitOne() |> ignore
-
-    //done
-    printfn "Timer ticked at %O" DateTime.Now.TimeOfDay
-
-printfn "userTimeWithCallback..."
-userTimerWithCallback
-printfn "userTimeWithCallback done!"
+    printfn "Doing something useful while waiting for write to complete"
+    // block on the timer now by waiting for the async to complete
+    Async.RunSynchronously async
+    // done
+    printfn "Async write completed"
